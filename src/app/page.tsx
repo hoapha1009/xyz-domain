b98dfe6df821432848fcc9cc2c4d4039c02263f8
HaFa <hoang.pham@buymed.com>
2024-02-29 10:10:21 +0700

    feat: check domain render page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ABC from '@/components/pages/abc'
+import Default from '@/components/pages/default'
 import XYZ from '@/components/pages/xyz'
 import { cookies } from 'next/headers'
 
@@ -6,13 +7,16 @@ import { cookies } from 'next/headers'
 export default function Home() {
   const cookieStore = cookies()
   console.log("🚀 ~ Home ~ cookieStore:", cookieStore)
-  const homePage = cookieStore.get('homePage')?.toString()
+  const homePage = cookieStore.get('homePage')
   const cookieList = cookieStore.getAll()
   console.log("🚀 ~ Home ~ cookieList:", cookieList)
-  console.log("🚀 ~ Home ~ homePage:", homePage)
+  console.log("🚀 ~ Home ~ homePage:", { homePage })
+  console.log("🚀 ~ Home ~ typeof homePage:", typeof homePage)
 
 
-  if (homePage === 'abc') return <ABC />
-  if (homePage === 'xyz') return <XYZ />
-  else return <div>Welcome to the default home page</div>;
+
+  if (homePage?.value === 'abc') return <ABC />
+  if (homePage?.value === 'xyz') return <XYZ />
+  if (homePage?.value === 'default') return <Default />
+  else return <>Hello</>
 }
---
+++ src/components/pages/default.tsx
@@ -0,0 +1,6 @@
+
+export default function Default() {
+    return (
+        <div>Welcome to the default home page</div>
+    )
+}
